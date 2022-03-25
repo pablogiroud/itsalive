d163c4b67ff2d6558026a68f1798013277ac513c
pablogiroud <pablogiroud@gmail.com>
2022-03-25 18:27:06 -0300

    fix uppercase issue

--- src/helpers/nameHandler.js
+++ src/helpers/nameHandler.js
@@ -1,8 +1,9 @@
 export const nameHandler = (val) => {
-    const arr = val.split(" ")
-    for(var i = 0; i < arr.length; i++) {
-        arr[i]=arr[i].charAt(0).toUpperCase() + arr[i].slice(1)
-    }
-    const nameCap = arr.join(" ")
-    return nameCap
+  let valUp = val.toUpperCase()
+  const arr = valUp.split(' ')
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1).toLowerCase()
+  }
+  const nameCap = arr.join(' ')
+  return nameCap
 }
